f0bca20eeca26538eecb94e38d89fd9ea5d4c8f2
Rivky Fisher <hand1b20@mbjcomp.org.il>
2024-05-05 12:38:48 +0300

    add environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase:{
+        apiKey: "AIzaSyB2vhrxOx9brHTKaIW5tkfWnRmVhtyMMIs",
+        authDomain: "ng-chat-e3210.firebaseapp.com",
+        projectId: "ng-chat-e3210",
+        storageBucket: "ng-chat-e3210.appspot.com",
+        messagingSenderId: "574910482235",
+        appId: "1:574910482235:web:a13744988a743b1bdad91e",
+        measurementId: "G-WBRBBX2BTP"
+    }
+  };
